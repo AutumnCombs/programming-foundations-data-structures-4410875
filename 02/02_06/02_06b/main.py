c929144c4d53b758cfd519eec27f2ad3d5dd8900
Autumn Combs <144552176+AutumnCombs@users.noreply.github.com>
2024-11-04 03:28:47 +0000

    adding tuple data

--- 02/02_06/02_06b/main.py
+++ 02/02_06/02_06b/main.py
@@ -1,1 +1,18 @@
 # Tuples are immutable array-like structures
+
+point = (5, 2)
+
+x = point[0]
+y = point[1]
+
+
+#using tuples to store orederd imuatable values
+#and to return multiple values from a function.
+def calculate_square_properties(side_length):
+  area = side_length * side_length
+  perimeter = 4 * side_length
+  return (area, perimeter)
+
+result = calculate_square_properties(5)
+print("Area: ", result[0])
+print("Perimeter ", result[1])
