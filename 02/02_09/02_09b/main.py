5cb03cf78a5a04de3b8d34f19bb88684f331ff09
Autumn Combs <144552176+AutumnCombs@users.noreply.github.com>
2024-11-04 04:19:08 +0000

    adding two solutions to the problem 1

--- 02/02_09/02_09b/main.py
+++ 02/02_09/02_09b/main.py
@@ -1,4 +1,14 @@
-def find_second_smallest(my_list):
-    return 0
+#def find_second_smallest(my_list):
+    #return 0
 
-print(find_second_smallest([5, 8, 3, 2, 6]))
+#print(find_second_smallest([5, 8, 3, 2, 6]))
+
+#print the array
+find_sec_smallest = [9, 3, 10, 2, 6]
+
+#sort the array
+print(sorted(find_sec_smallest, reverse=True))
+
+#print the 2nd to last index in the array
+#if it is desc
+print(find_sec_smallest[1])
--- 02/02_09/02_09e/main.py
+++ 02/02_09/02_09e/main.py
@@ -1,4 +1,26 @@
+#option 1, sorted function built off timsort
+#built from merge sort/ insertion sort
+#time complexity: n log n 
 def find_second_smallest(my_list):
-    return 0
+    if len(my_list) < 2:
+        return None
+    sorted_list = sorted(my_list)
+    return sorted_list[1]
 
-print(find_second_smallest([5, 8, 3, 2, 6]))
+#custom alg to find 2nd smallest item
+#time complexity is On b/c you only iterate through once
+def find_second_smallest_v2(my_list):
+    if len(my_list) < 2:
+        return None
+    smallest = float('inf')
+    second_smallest = float('inf')
+
+    for num in my_list:
+        if num < smallest:
+            second_smallest = smallest
+            smallest = num
+        elif num < second_smallest:
+            second_smallest = num
+    return second_smallest
+
+print(find_second_smallest_v2([5, 8, 3, 2, 6]))
