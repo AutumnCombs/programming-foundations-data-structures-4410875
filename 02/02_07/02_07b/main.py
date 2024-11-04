28e3f579c4d9b6bb17f08d184d27784c3264b55f
Autumn Combs <144552176+AutumnCombs@users.noreply.github.com>
2024-11-04 03:34:10 +0000

    adding comments

--- 02/02_07/02_07b/main.py
+++ 02/02_07/02_07b/main.py
@@ -1,1 +1,22 @@
-# Linear Search
+# Linear Search or Sequential Search
+# Brute Force Method (simple but slow)
+
+my_list = [8, 5, 0, 3, 9, 7]
+ITEM = 7
+
+#function that searches for an item in the list
+#if you find an item return true, if you make it through
+#the entire structure without finding one return false
+def search(item, listy):
+    for element in listy:
+        if element == item:
+            return True
+    return False
+
+
+#brute force search that is very slow
+#linear search increases with the size of the array/list 
+#time complexity of On
+print(search(ITEM, my_list))
+
+ITEM_INDEX = my_list.index(ITEM)
